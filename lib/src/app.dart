b1634f0c51b7da86013840335c46899fc1a03291
FlutLab.io <>
2022-06-29 12:24:48 +0000

    Autosave

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'ui/accueil.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: const Accueil()
-      ),
-    );
+    return MaterialApp(theme: ThemeData.dark(), home: const Accueil());
   }
 }
