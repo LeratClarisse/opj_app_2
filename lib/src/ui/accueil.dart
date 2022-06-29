9c9d1e0d47ac423e04405d78c3b5c315a84246a3
FlutLab.io <>
2022-06-29 19:27:30 +0000

    Autosave

--- lib/src/ui/accueil.dart
+++ lib/src/ui/accueil.dart
@@ -40,8 +40,7 @@ class Accueil extends StatelessWidget {
             ListTile(
               title: const Text('Cours'),
               onTap: () {
-                Navigator.push(
-                  context,
+                Navigator.of(context).push(
                   MaterialPageRoute(builder: (context) => const Cours()),
                 );
               },
