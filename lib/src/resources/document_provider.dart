6c4db4b7327cb5f3c11bc16e066ad1bfaf0d4c87
FlutLab.io <>
2022-06-30 18:50:49 +0000

    Autosave

--- lib/src/resources/document_provider.dart
+++ lib/src/resources/document_provider.dart
@@ -4,11 +4,13 @@ import '../models/document.dart';
 
 class DocumentProvider {
   Future<List<Document>> fetchDocumentList() async {
-    const response = '{"items": [{"id": "1", "docNumber": "1", "title": "Doc1", "file": "doc1.pdf", "category": "DPG" },{"id": "2", "docNumber": "2", "title": "Doc2", "file": "doc2.pdf", "category": "DPS" },{"id": "3", "docNumber": "3", "title": "Doc3", "file": "doc3.pdf", "category": "DPG" },{"id": "4", "docNumber": "4", "title": "Doc4", "file": "doc4.pdf", "category": "DPS" },{"id": "5", "docNumber": "5", "title": "Doc5", "file": "doc5.pdf", "category": "PP" }]}';
+    const response = '{"documents": [{"id": "1", "docNumber": "1", "title": "Doc1", "file": "doc1.pdf", "category": "DPG" },{"id": "2", "docNumber": "2", "title": "Doc2", "file": "doc2.pdf", "category": "DPS" },{"id": "3", "docNumber": "3", "title": "Doc3", "file": "doc3.pdf", "category": "DPG" },{"id": "4", "docNumber": "4", "title": "Doc4", "file": "doc4.pdf", "category": "DPS" },{"id": "5", "docNumber": "5", "title": "Doc5", "file": "doc5.pdf", "category": "PP" }]}';
 
     // ignore: avoid_print
-    print(json.decode(response).items);
+    print(json.decode(response));
     if (response.isNotEmpty) {
+      //Iterable l = json.decode(response.body);
+      //List<Document> documents = List<Document>.from(l.map((model) => Document.fromJson(model)));
       return [];
     } else {
       throw Exception('Failed to load documents');
