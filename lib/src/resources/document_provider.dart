d5e2e61568d49023584375e2e8d41922478220fd
FlutLab.io <>
2022-06-30 19:47:42 +0000

    Autosave

--- lib/src/resources/document_provider.dart
+++ lib/src/resources/document_provider.dart
@@ -3,8 +3,8 @@ import 'dart:convert';
 import '../models/document.dart';
 
 class DocumentProvider {
-  final documentsJson = '{"documents": [{"id": 1, "docNumber": 1, "title": "Doc1", "file": "doc1.pdf", "category": "DPG" },{"id": 2, "docNumber": 2, "title": "Doc2", "file": "doc2.pdf", "category": "DPS" },{"id": 3, "docNumber": 3, "title": "Doc3", "file": "doc3.pdf", "category": "DPG" },{"id": 4, "docNumber": 4, "title": "Doc4", "file": "doc4.pdf", "category": "DPS" },{"id": 5, "docNumber": 5, "title": "Doc5", "file": "doc5.pdf", "category": "PP"},{"id": 6, "docNumber": 6, "title": "Recap1", "file": "recap1.pdf", "category": "DPG" },{"id": 7, "docNumber": 7, "title": "Recap2", "file": "recap2.pdf", "category": "DPS" }]}';
-  final sumupsJson = '{"sumups": [{"id": 1, "docNumber": 1, "title": "Doc1", "file": "doc1.pdf", "category": "DPG" },{"id": 2, "docNumber": 2, "title": "Doc2", "file": "doc2.pdf", "category": "DPS" },{"id": 3, "docNumber": 3, "title": "Doc3", "file": "doc3.pdf", "category": "DPG" },{"id": 4, "docNumber": 4, "title": "Doc4", "file": "doc4.pdf", "category": "DPS" },{"id": 5, "docNumber": 5, "title": "Doc5", "file": "doc5.pdf", "category": "PP"},{"id": 6, "docNumber": 6, "title": "Recap1", "file": "recap1.pdf", "category": "DPG" },{"id": 7, "docNumber": 7, "title": "Recap2", "file": "recap2.pdf", "category": "DPS" }]}';
+  final documentsJson = '{"documents": [{"id": 1, "docNumber": 1, "title": "Doc1", "file": "doc1.pdf", "category": "DPG" },{"id": 2, "docNumber": 2, "title": "Doc2", "file": "doc2.pdf", "category": "DPS" },{"id": 3, "docNumber": 3, "title": "Doc3", "file": "doc3.pdf", "category": "DPG" },{"id": 4, "docNumber": 4, "title": "Doc4", "file": "doc4.pdf", "category": "DPS" },{"id": 5, "docNumber": 5, "title": "Doc5", "file": "doc5.pdf", "category": "PP"}]}';
+  final sumupsJson = '{"sumups": [{"id": 6, "docNumber": 6, "title": "Recap1", "file": "recap1.pdf", "category": "DPG" },{"id": 7, "docNumber": 7, "title": "Recap2", "file": "recap2.pdf", "category": "DPS" }]}';
 
   Future<List<Document>> fetchCourseList() async {
     if (documentsJson.isNotEmpty) {
