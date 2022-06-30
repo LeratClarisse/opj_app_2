946518877c52bffeffdc81078b2ebeccaff0c1d0
FlutLab.io <>
2022-06-30 19:41:47 +0000

    Autosave

--- lib/src/models/document.dart
+++ lib/src/models/document.dart
@@ -7,7 +7,7 @@ class Document {
   final String? subcategory;
   final bool isSumUp;
 
-  const Document({required this.id, required this.docNumber, required this.title, required this.file, required this.category, this.subcategory, this.isSumUp = false});
+  const Document({required this.id, required this.docNumber, required this.title, required this.file, required this.category, this.subcategory, required this.isSumUp});
 
   factory Document.fromJson(Map<String, dynamic> json) {
     return Document(id: json['id'], docNumber: json['docNumber'], title: json['title'], file: json['file'], category: json['category'], subcategory: json['subcategory'], isSumUp: json['isSumUp']);
