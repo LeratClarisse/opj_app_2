f3cfd60762514192c5a4da518373933d96d6b1a7
FlutLab.io <>
2022-06-30 19:47:02 +0000

    Autosave

--- lib/src/resources/document_provider.dart
+++ lib/src/resources/document_provider.dart
@@ -3,16 +3,13 @@ import 'dart:convert';
 import '../models/document.dart';
 
 class DocumentProvider {
-  final response = '{"documents": [{"id": 1, "docNumber": 1, "title": "Doc1", "file": "doc1.pdf", "category": "DPG", "isSumUp": false },{"id": 2, "docNumber": 2, "title": "Doc2", "file": "doc2.pdf", "category": "DPS", "isSumUp": false },{"id": 3, "docNumber": 3, "title": "Doc3", "file": "doc3.pdf", "category": "DPG", "isSumUp": false },{"id": 4, "docNumber": 4, "title": "Doc4", "file": "doc4.pdf", "category": "DPS", "isSumUp": false },{"id": 5, "docNumber": 5, "title": "Doc5", "file": "doc5.pdf", "category": "PP", "isSumUp": false},{"id": 6, "docNumber": 6, "title": "Recap1", "file": "recap1.pdf", "category": "DPG", "isSumUp": true },{"id": 7, "docNumber": 7, "title": "Recap2", "file": "recap2.pdf", "category": "DPS", "isSumUp": true }]}';
+  final documentsJson = '{"documents": [{"id": 1, "docNumber": 1, "title": "Doc1", "file": "doc1.pdf", "category": "DPG" },{"id": 2, "docNumber": 2, "title": "Doc2", "file": "doc2.pdf", "category": "DPS" },{"id": 3, "docNumber": 3, "title": "Doc3", "file": "doc3.pdf", "category": "DPG" },{"id": 4, "docNumber": 4, "title": "Doc4", "file": "doc4.pdf", "category": "DPS" },{"id": 5, "docNumber": 5, "title": "Doc5", "file": "doc5.pdf", "category": "PP"},{"id": 6, "docNumber": 6, "title": "Recap1", "file": "recap1.pdf", "category": "DPG" },{"id": 7, "docNumber": 7, "title": "Recap2", "file": "recap2.pdf", "category": "DPS" }]}';
+  final sumupsJson = '{"sumups": [{"id": 1, "docNumber": 1, "title": "Doc1", "file": "doc1.pdf", "category": "DPG" },{"id": 2, "docNumber": 2, "title": "Doc2", "file": "doc2.pdf", "category": "DPS" },{"id": 3, "docNumber": 3, "title": "Doc3", "file": "doc3.pdf", "category": "DPG" },{"id": 4, "docNumber": 4, "title": "Doc4", "file": "doc4.pdf", "category": "DPS" },{"id": 5, "docNumber": 5, "title": "Doc5", "file": "doc5.pdf", "category": "PP"},{"id": 6, "docNumber": 6, "title": "Recap1", "file": "recap1.pdf", "category": "DPG" },{"id": 7, "docNumber": 7, "title": "Recap2", "file": "recap2.pdf", "category": "DPS" }]}';
 
   Future<List<Document>> fetchCourseList() async {
-    if (response.isNotEmpty) {
-      Iterable l = json.decode(response)['documents'];
-      List<Document> documents = List<Document>.from(l.map((model) {
-        if (!model['isSumUp']) {
-          Document.fromJson(model);
-        }
-      }));
+    if (documentsJson.isNotEmpty) {
+      Iterable l = json.decode(documentsJson)['documents'];
+      List<Document> documents = List<Document>.from(l.map((model) => Document.fromJson(model)));
       return documents;
     } else {
       throw Exception('Failed to load documents');
@@ -20,14 +17,10 @@ class DocumentProvider {
   }
 
   Future<List<Document>> fetchSumUpList() async {
-    if (response.isNotEmpty) {
-      Iterable l = json.decode(response)['documents'];
-      List<Document> documents = List<Document>.from(l.map((model) {
-        if (model['isSumUp']) {
-          Document.fromJson(model);
-        }
-      }));
-      return documents;
+    if (sumupsJson.isNotEmpty) {
+      Iterable l = json.decode(sumupsJson)['sumups'];
+      List<Document> sumups = List<Document>.from(l.map((model) => Document.fromJson(model)));
+      return sumups;
     } else {
       throw Exception('Failed to load documents');
     }
