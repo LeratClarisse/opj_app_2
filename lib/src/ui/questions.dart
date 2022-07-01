0dd36c7b3026757083c947471776a6c1f04be267
FlutLab.io <>
2022-07-01 21:53:46 +0000

    Autosave

--- lib/src/ui/questions.dart
+++ lib/src/ui/questions.dart
@@ -35,7 +35,7 @@ class _Questions extends State<Questions> {
         width: selected ? 100.0 : 200.0,
         height: selected ? 100.0 : 200.0,
         top: selected ? 50.0 : 150.0,
-        duration: const Duration(milliseconds: 100),
+        duration: const Duration(milliseconds: 300),
         curve: Curves.fastOutSlowIn,
         child: GestureDetector(
           child: Container(
