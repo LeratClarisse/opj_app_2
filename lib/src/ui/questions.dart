819b0cc86f374deb89f91ef9095f159b013e5d19
FlutLab.io <>
2022-07-01 21:12:09 +0000

    Autosave

--- lib/src/ui/questions.dart
+++ lib/src/ui/questions.dart
@@ -26,29 +26,26 @@ class _Questions extends State<Questions> {
   }
 
   Widget buildQuestion(BuildContext context) {
-    return SizedBox(
-        width: 200,
-        height: 350,
-        child: Stack(children: <Widget>[
-          AnimatedPositioned(
-            width: selected ? 200.0 : 50.0,
-            height: selected ? 50.0 : 200.0,
-            top: selected ? 50.0 : 150.0,
-            duration: const Duration(seconds: 1),
-            curve: Curves.fastOutSlowIn,
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  selected = !selected;
-                });
-              },
-              child: Container(
-                color: Colors.blue,
-                child: const Center(child: Text('Tap me')),
-              ),
-            ),
-          )
-        ]));
+    return Stack(children: <Widget>[
+      AnimatedPositioned(
+        width: selected ? 200.0 : 50.0,
+        height: selected ? 50.0 : 200.0,
+        top: selected ? 50.0 : 150.0,
+        duration: const Duration(seconds: 1),
+        curve: Curves.fastOutSlowIn,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              selected = !selected;
+            });
+          },
+          child: Container(
+            color: Colors.blue,
+            child: const Center(child: Text('Tap me')),
+          ),
+        ),
+      )
+    ]);
   }
 
   /// Bottom side rendering (buttons)
