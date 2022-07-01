9a0f8204bb13ae4e4ac844b5ae041e2045ef0c74
FlutLab.io <>
2022-07-01 22:09:35 +0000

    Autosave

--- lib/src/ui/questions.dart
+++ lib/src/ui/questions.dart
@@ -55,7 +55,7 @@ class _Questions extends State<Questions> {
   Widget buildReponse(BuildContext context) {
     return AnimatedOpacity(
       opacity: opacityLevel,
-      duration: const Duration(seconds: 1),
+      duration: const Duration(milliseconds: 500),
       child: Container(
         width: selected ? 200 : 0,
         height: selected ? 200 : 0,
