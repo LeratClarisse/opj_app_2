e1bc79ada8ee18ede9ef13978251ed1d8a8275e6
FlutLab.io <>
2022-07-01 13:00:50 +0000

    boutons bas

--- lib/src/ui/questions.dart
+++ lib/src/ui/questions.dart
@@ -6,29 +6,34 @@ class Questions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle style = ElevatedButton.styleFrom(
-      textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      fixedSize: const Size(200, 50),
-      padding: const EdgeInsets.all(20),
-    );
-
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: const Text('OPJ Expert'),
         ),
         drawer: const Menu(),
-        body: Align(
-            alignment: Alignment.bottomCenter,
-            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-              ElevatedButton(style: style, onPressed: () {}, child: const Text('Réponse')),
-              const SizedBox(height: 30),
-              Row(children: <Widget>[
-                Expanded(child: IconButton(icon: const Icon(Icons.arrow_left), onPressed: () {})),
-                Expanded(flex: 2, child: IconButton(icon: const Icon(Icons.document_scanner_outlined), onPressed: () {})),
-                Expanded(child: IconButton(icon: const Icon(Icons.arrow_right), onPressed: () {}))
-              ]),
-              const SizedBox(height: 30),
-            ])));
+        body: buildBottom(context));
+  }
+
+  /// Bottom side rendering (buttons)
+  Widget buildBottom(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      fixedSize: const Size(200, 50),
+      padding: const EdgeInsets.all(20),
+    );
+
+    return Align(
+        alignment: Alignment.bottomCenter,
+        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          ElevatedButton(style: style, onPressed: () {}, child: const Text('Réponse')),
+          const SizedBox(height: 30),
+          Row(children: <Widget>[
+            Expanded(child: IconButton(icon: const Icon(Icons.arrow_left), onPressed: () {})),
+            Expanded(flex: 2, child: IconButton(icon: const Icon(Icons.document_scanner_outlined), onPressed: () {})),
+            Expanded(child: IconButton(icon: const Icon(Icons.arrow_right), onPressed: () {}))
+          ]),
+          const SizedBox(height: 30),
+        ]));
   }
 }
