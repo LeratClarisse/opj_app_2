42297c64d249a3233b45ae2675d2e1b9bfbf374e
FlutLab.io <>
2022-07-01 12:28:19 +0000

    Autosave

--- lib/src/ui/questions.dart
+++ lib/src/ui/questions.dart
@@ -24,9 +24,9 @@ class Questions extends StatelessWidget {
               ElevatedButton(style: style, onPressed: () {}, child: const Text('Réponse')),
               const SizedBox(height: 30),
               Row(children: <Widget>[
-                Expanded(flex: 2, child: IconButton(icon: const Icon(Icons.arrow_left), tooltip: 'Préc.', onPressed: () {})),
-                Expanded(child: ElevatedButton(style: style, onPressed: () {}, child: const Text('Fiche'))),
-                Expanded(flex: 1, child: IconButton(icon: const Icon(Icons.arrow_right), tooltip: 'Suiv.', onPressed: () {}))
+                Expanded(child: IconButton(icon: const Icon(Icons.arrow_left), tooltip: 'Préc.', onPressed: () {})),
+                Expanded(flex: 2, child: ElevatedButton(style: style, onPressed: () {}, child: const Text('Fiche'))),
+                Expanded(child: IconButton(icon: const Icon(Icons.arrow_right), tooltip: 'Suiv.', onPressed: () {}))
               ]),
               const SizedBox(height: 30),
             ])));
