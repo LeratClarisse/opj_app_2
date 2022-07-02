c68a94e745cc8edd42d2a4c053381426da7aafdf
FlutLab.io <>
2022-07-02 08:29:37 +0000

    Autosave

--- lib/src/blocs/question_bloc.dart
+++ lib/src/blocs/question_bloc.dart
@@ -6,28 +6,22 @@ import 'dart:math';
 class QuestionsBloc {
   final _repository = Repository();
   int _nbQuestions = 0;
-  final _questionsFetcher = PublishSubject<List<Question>>();
+  List<Question> questions = [];
   final _randomQuestionFetcher = PublishSubject<Question>();
 
-  Stream<List<Question>> get allQuestions => _questionsFetcher.stream;
   Stream<Question> get randomQuestion => _randomQuestionFetcher.stream;
 
-  fetchAllQuestions() async {
-    List<Question> questions = await _repository.fetchAllQuestions();
-    // ignore: avoid_print
-    print("test");
-    _nbQuestions = questions.length;
-    _questionsFetcher.sink.add(questions);
-  }
-
   fetchRandomQuestion() async {
-    int randomId = 1 + Random().nextInt(_nbQuestions - 1);
-    Question question = await _repository.fetchQuestionById(randomId);
-    _randomQuestionFetcher.sink.add(question);
+    questions = await _repository.fetchAllQuestions();
+    _nbQuestions = questions.length;
+    if (_nbQuestions == 0 || _nbQuestions == -1) {
+      int randomId = 1 + Random().nextInt(_nbQuestions - 1);
+      Question question = await _repository.fetchQuestionById(randomId);
+      _randomQuestionFetcher.sink.add(question);
+    }
   }
 
   dispose() {
-    _questionsFetcher.close();
     _randomQuestionFetcher.close();
   }
 }
