e9c2a1ccc4d70a6088dd44a0da32b89d77fbbdce
FlutLab.io <>
2022-07-04 19:13:37 +0000

    Autosave

--- lib/src/resources/question_provider.dart
+++ lib/src/resources/question_provider.dart
@@ -4,15 +4,18 @@ import '../models/question.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 
 class QuestionProvider {
   final questionsJson = '{"questions": [{"id": 1, "docNumber": 1, "label": "Question1", "response": "Réponse1", "category": "DPG" },{"id": 2, "docNumber": 2, "label": "Question2", "response": "Réponse2", "category": "DPS" },{"id": 3, "docNumber": 3, "label": "Question3", "response": "Réponse3", "category": "DPG" },{"id": 4, "docNumber": 4, "label": "Question4", "response": "Réponse4", "category": "DPS" },{"id": 5, "docNumber": 5, "label": "Question5", "response": "Réponse5", "category": "PP"}]}';
-  final database = openDatabase(
-    join('../../../assets/db', 'opj_db.db'),
-  );
 
   Future<int> fetchNbQuestions() async {
     if (!kIsWeb) {
+      WidgetsFlutterBinding.ensureInitialized();
+      final database = openDatabase(
+        join(await getDatabasesPath(), 'db/opj_db.db'),
+      );
+
       Database db = await database;
       // Convert the List<Map<String, dynamic> into a List<Question>
       int? nb = Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM Question'));
@@ -30,6 +33,11 @@ class QuestionProvider {
 
   Future<Question> fetchQuestionById(int id) async {
     if (!kIsWeb) {
+      WidgetsFlutterBinding.ensureInitialized();
+      final database = openDatabase(
+        join(await getDatabasesPath(), 'db/opj_db.db'),
+      );
+
       Database db = await database;
       // Query the table for all The Questions
       final List<Map<String, dynamic>> maps = await db.query('question',
