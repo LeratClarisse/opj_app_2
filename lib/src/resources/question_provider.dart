b2ddb0dbdb4ab6f8bbc029cbb236398e388ebdfa
FlutLab.io <>
2022-07-04 17:01:14 +0000

    Autosave

--- lib/src/resources/question_provider.dart
+++ lib/src/resources/question_provider.dart
@@ -8,7 +8,7 @@ import 'package:flutter/foundation.dart';
 class QuestionProvider {
   final questionsJson = '{"questions": [{"id": 1, "docNumber": 1, "label": "Question1", "response": "Réponse1", "category": "DPG" },{"id": 2, "docNumber": 2, "label": "Question2", "response": "Réponse2", "category": "DPS" },{"id": 3, "docNumber": 3, "label": "Question3", "response": "Réponse3", "category": "DPG" },{"id": 4, "docNumber": 4, "label": "Question4", "response": "Réponse4", "category": "DPS" },{"id": 5, "docNumber": 5, "label": "Question5", "response": "Réponse5", "category": "PP"}]}';
   final database = openDatabase(
-    join('../../assets/db', 'opj_db.db'),
+    join('../../../assets/db', 'opj_db.db'),
   );
 
   Future<int> fetchNbQuestions() async {
