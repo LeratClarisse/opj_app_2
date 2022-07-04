a4d32f92decbe366c68030df8b5c567141e05604
FlutLab.io <>
2022-07-04 19:54:47 +0000

    Autosave

--- lib/src/resources/question_provider.dart
+++ lib/src/resources/question_provider.dart
@@ -5,7 +5,6 @@ import '../models/question.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' as path;
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
