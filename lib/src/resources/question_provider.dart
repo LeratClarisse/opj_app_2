8be87198ff90f73101ba0e586c02f9fd8093d102
FlutLab.io <>
2022-07-04 15:23:03 +0000

    Autosave

--- lib/src/resources/question_provider.dart
+++ lib/src/resources/question_provider.dart
@@ -1,34 +1,56 @@
 import 'dart:async';
+import 'dart:convert';
 import '../models/question.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:flutter/foundation.dart';
 
 class QuestionProvider {
+  final questionsJson = '{"questions": [{"id": 1, "docNumber": 1, "label": "Question1", "response": "Réponse1", "category": "DPG" },{"id": 2, "docNumber": 2, "label": "Question2", "response": "Réponse2", "category": "DPS" },{"id": 3, "docNumber": 3, "label": "Question3", "response": "Réponse3", "category": "DPG" },{"id": 4, "docNumber": 4, "label": "Question4", "response": "Réponse4", "category": "DPS" },{"id": 5, "docNumber": 5, "label": "Question5", "response": "Réponse5", "category": "PP"}]}';
   final database = openDatabase(
     join('../../assets/db', 'opj_db.db'),
   );
 
   Future<int> fetchNbQuestions() async {
-    Database db = await database;
-
-    // Convert the List<Map<String, dynamic> into a List<Question>
-    int? nb = Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM Question'));
-
-    // Return COUNT or 0
-    return nb ??= 0;
+    if (!kIsWeb) {
+      Database db = await database;
+      // Convert the List<Map<String, dynamic> into a List<Question>
+      int? nb = Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM Question'));
+      // Return COUNT or 0
+      return nb ??= 0;
+    } else {
+      if (questionsJson.isNotEmpty) {
+        Iterable l = json.decode(questionsJson)['questions'];
+        return l.length;
+      } else {
+        throw Exception('Failed to load questions');
+      }
+    }
   }
 
   Future<Question> fetchQuestionById(int id) async {
-    Database db = await database;
-
-    // Query the table for all The Questions
-    final List<Map<String, dynamic>> maps = await db.query('question',
-        where: "id = ?",
-        whereArgs: [
-          id
-        ],
-        limit: 1);
-
-    return Question.fromJson(maps[0]);
+    if (!kIsWeb) {
+      Database db = await database;
+      // Query the table for all The Questions
+      final List<Map<String, dynamic>> maps = await db.query('question',
+          where: "id = ?",
+          whereArgs: [
+            id
+          ],
+          limit: 1);
+      return Question.fromJson(maps[0]);
+    } else {
+      if (questionsJson.isNotEmpty) {
+        Iterable l = json.decode(questionsJson)['questions'];
+        List<Question> question = List<Question>.from(l.map((model) {
+          if (model['id'] == id) {
+            Question.fromJson(model);
+          }
+        }));
+        return question[0];
+      } else {
+        throw Exception('Failed to load question ' + id.toString());
+      }
+    }
   }
 }
