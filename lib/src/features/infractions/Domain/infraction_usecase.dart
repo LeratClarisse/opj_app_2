7edb188451b30f328a82c1f9a189b82c8530464a
Lerat Clarisse <clarisselerat@hotmail.fr>
2023-02-22 09:19:22 +0000

    infractions domain

---
+++ lib/src/features/infractions/Domain/infraction_usecase.dart
@@ -0,0 +1,20 @@
+import 'package:opjapp/src/features/infractions/Data/infraction_repository.dart';
+import 'package:opjapp/src/features/infractions/Domain/infraction_entity.dart';
+
+class InfractionUsecase {
+  List<InfractionEntity> _infractions = [];
+  final _repository = InfractionRepository();
+
+  fetchAllInfractions() {
+    _infractions = _repository.fetchAllInfractions();
+    return _infractions;
+  }
+
+  searchDps({String search = ''}) {
+    if (search.isEmpty) {
+      return _infractions;
+    } else {
+      return _infractions.where((q) => q.dpsLongLabel!.toLowerCase().contains(search.toLowerCase())).toList();
+    }
+  }
+}
