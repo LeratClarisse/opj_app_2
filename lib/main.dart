7ff8867284cbb350255881118e09deb85c3449e3
Lerat Clarisse <clarisselerat@hotmail.fr>
2023-03-10 09:45:46 +0000

    Hive clear when update + Remove useless file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'src/core/Presentation/app.dart';
 
 Future<void> main() async {
   await Hive.initFlutter();
+
+  // App version from pubspec.yaml
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  String appVersion = packageInfo.version;
+  // App version from Hive
+  Box settingBox = await Hive.openBox('settings');
+  String? boxAppVersion = settingBox.get('version');
+  await settingBox.close();
+
+  if (appVersion != boxAppVersion) {
+    Hive.openBox('questions');
+    Hive.deleteFromDisk(); // Delete open boxes
+
+    // Save new version in Hive
+    (await Hive.openBox('settings')).put('version', appVersion);
+  }
+
   return runApp(const App());
 }
